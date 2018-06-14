481bcb209ed36b3f03b030ada23b35310fa8f748
Bjoern Schumacher <schumibjoern@gmaill.com>
2018-06-14 14:56:47 +0200

    Added dependencies and files for webpack, react-dom and typescript

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
-var express = require('express')
-var app = express()
+'use strict';
+var path = require('path');
+var express = require('express');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+var app = express();
 
-app.listen(3000)
+var staticPath = path.join(__dirname, '/');
+app.use(express.static(staticPath));
+
+// Allows you to set port in the project properties.
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('listening');
+});
